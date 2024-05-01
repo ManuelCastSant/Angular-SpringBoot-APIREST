ee42afb92b5bbc6eb24366b2ee557bff1a592ae5
ManuelCastSant <manu.casa1998@gmail.com>
2024-05-01 14:32:35 -0600

    yes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,12 +5,22 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,CommonModule],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title: string = 'Hola mundo Angular desde componente!';
-  users: string[] = ['Pepe', 'Maria', 'Juan', 'Andres','Manuel'];
+
+  users: string[] = ['Pepe', 'Maria', 'Juan', 'Andres', 'Manuel'];
   //users: string[] = [];
+
+  //Visible
+  visible: boolean = false;
+
+  //Metodo
+  setVisible(): void {
+    this.visible = this.visible? false : true;
+    console.log('Hemos hecho clic en setVisible');
+  }
 }
